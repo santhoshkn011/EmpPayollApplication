053e6fc4ebd965fdb51bb0419401ce88862adaa3
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-11 17:31:13 +0530

    Added: Ability to Set Event Listener on Salary Range to display appropriate value

---
+++ js/empForm.js
@@ -0,0 +1,7 @@
+//UC8 : Ability to Set Event Listener on Salary Range to display appropriate value 
+const salary=document.querySelector('#salary');
+const output=document.querySelector('.salary-output');
+output.textContent=salary.value;
+salary.addEventListener('input',function(){
+    output.textContent=salary.value;
+    })
