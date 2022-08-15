d2b4159fd418df487ce751f2ce49a833a36bcb00
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-15 11:56:25 +0530

    Added: Employee Home Page

--- js/empForm.js
+++ js/empForm.js
@@ -1,27 +1,65 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const name = document.querySelector('#name');
-    const textError = document.querySelector('.text-error');
-    name.addEventListener('input', function() {
-        if (name.value.length == 0) {
-            textError.textContent = " ";
-            return;
-        }
-        try {
-            (new EmployeePayrollData()).name = name.value;
-            textError.textContent = " ";
-        } catch (e) {
-            textError.textContent = e;
-        }
-    });
+    salaryOutput();
+    validateName();
+    validateDate();
 
-    const salary = document.querySelector("#salary");
+});
+
+function salaryOutput() {
+    const salary = document.querySelector('#salary');
     const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
     salary.addEventListener('input', function() {
         output.textContent = salary.value;
-    });
 
-});
+    });
+}
+
+function validateName() {
+    let name = document.querySelector('#name');
+    let textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if (nameRegex.test(name.value)) {
+            textError.textContent = "";
+        } else {
+            textError.textContent = "Name is Incorrect"
+        }
+    });
+}
+
+function validateDate() {
+    let day = document.querySelector('#day');
+    let month = document.querySelector('#month');
+    let year = document.querySelector('#year');
+    day.addEventListener('input', checkDate);
+    month.addEventListener('input', checkDate);
+    year.addEventListener('input', checkDate);
+}
+
+function checkDate() {
+    let dateError = document.querySelector('.dates-error');
+    let date = day.value + " " + month.value + " " + year.value;
+    try {
+        checkStartDate(new Date(Date.parse(date)));
+        dateError.textContent = "";
+    } catch (e) {
+        dateError.textContent = e;
+    }
+
+}
+
+function checkStartDate(startDate) {
+    let currentDate = new Date();
+    if (startDate > currentDate) {
+        throw new Error("Start date is a future date")
+    }
+    let differnce = Math.abs(currentDate.getTime() - startDate.getTime());
+    let date = differnce / (1000 * 60 * 60 * 24);
+    if (date <30) {
+        throw new Error("Start date is beyond 30 days");
+    }
+}
 
 function save(event) {
     alert("save")
@@ -86,8 +124,8 @@ function createAndUpdateStorage(employeePayrollData) {
     } else {
         employeePayrollList.push(employeePayrollData);
     }
-    alert(employeePayrollList.toString());
-    localStorage.setItem("EmployeePayrollListList", JSON.stringify(employeePayrollList));
+    //alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
 
 function resetButton() {
